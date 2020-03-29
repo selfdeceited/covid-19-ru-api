7768b0d95b3908bfe81493047a6bac01bd766d73
selfdeceited <selfdeceited@gmail.com>
2020-03-29 13:22:40 +0300

    test fixes

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataRetrieverService } from './data-retriever/data-retriever.service';
+import { of } from 'rxjs';
 
 describe('AppController', () => {
   let appController: AppController;
+  const dataRetrieverService = {
+    fetchForDate: function() {
+      return of({data: ``})
+    }
+  };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+      providers: [AppService, DataRetrieverService],
+    })
+    .overrideProvider(DataRetrieverService)
+    .useValue(dataRetrieverService)
+    .compile();
 
     appController = app.get<AppController>(AppController);
   });
--- src/data-retriever/data-retriever.service.spec.ts
+++ src/data-retriever/data-retriever.service.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataRetrieverService } from './data-retriever.service';
+import { HttpService, HttpModule } from '@nestjs/common';
 
 describe('DataRetrieverService', () => {
   let service: DataRetrieverService;
-
+  const httpService = {}
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [DataRetrieverService],
-    }).compile();
+    })
+    .overrideProvider(HttpService)
+    .useValue(httpService)
+    .compile();
 
     service = module.get<DataRetrieverService>(DataRetrieverService);
   });
