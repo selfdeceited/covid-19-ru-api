ad912705e94fe3c390385ae565d20aa1fc8c9fab
selfdeceited <selfdeceited@gmail.com>
2020-03-30 22:59:30 +0300

    coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   reporters: ['default', 'jest-junit'],
+  collectCoverage: true,
+  coverageReporters: [
+    "text",
+    "cobertura"
+  ],
 }
